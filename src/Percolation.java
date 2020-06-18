3193b5fc1f2c403ca43073aeda589e0891893131
iceihehe <iceihehe@gmail.com>
2020-06-18 21:45:01 +0800

    done

--- src/Percolation.java
+++ src/Percolation.java
@@ -1,4 +1,3 @@
-import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 
--- src/PercolationStats.java
+++ src/PercolationStats.java
@@ -1,7 +1,6 @@
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 
-import java.util.Arrays;
 
 
 public class PercolationStats {
@@ -58,18 +57,18 @@ public class PercolationStats {
         return mean() + 1.96 * Math.sqrt(stddev() / T);
     }
     public static void main(String[] args) {
-//        if (args.length != 2) throw new IllegalArgumentException();
-//        int n = Integer.parseInt(args[0]);
-//        int t = Integer.parseInt(args[1]);
-        int n = 200;
-        int t = 100;
+        if (args.length != 2) throw new IllegalArgumentException();
+        int n = Integer.parseInt(args[0]);
+        int t = Integer.parseInt(args[1]);
 
         if (n <= 0 || t <= 0) throw new IllegalArgumentException();
+
         PercolationStats ps = new PercolationStats(n, t);
         ps.simulate();
-        System.out.println(String.format("mean\t\t= %s", ps.mean()));
-        System.out.println(String.format("stddev\t\t= %s", ps.stddev()));
-        System.out.println(String.format("95%% confidence interval\t\t= [%s, %2$s]", ps.confidenceLo(), ps.confidenceHi()));
+        System.out.println(String.format("mean\t\t\t\t\t\t\t= %s", ps.mean()));
+        System.out.println(String.format("stddev\t\t\t\t\t\t\t= %s", ps.stddev()));
+        System.out.println(String.format("95%% confidence interval\t\t\t= [%s, %2$s]", ps.confidenceLo(), ps.confidenceHi()));
+
 
     }
 }
