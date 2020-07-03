8817129843462726bef0b651378ec5c0b1044f4d
iceihehe <iceihehe@gmail.com>
2020-07-03 17:25:29 +0800

    fix

--- src/Point.java
+++ src/Point.java
@@ -8,7 +8,6 @@
  *
  ******************************************************************************/
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Comparator;
 import edu.princeton.cs.algs4.StdDraw;
